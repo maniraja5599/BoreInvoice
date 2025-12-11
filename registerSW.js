a4d435c18dbe9c17453f81e7e84e6ad0f848c3bc
maniraja5599 <manirajankg@gmail.com>
2025-12-11 06:19:07 +0000

    Deploying to gh-pages from @ maniraja5599/BoreInvoice@0baee076d4bde8e7d191121e4773d1ccb8a9bbd2 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/BoreInvoice/sw.js', { scope: '/BoreInvoice/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
